ee74c61b82c86d54b142aebaa0c78371f2cb7802
Fay Vera <fayverawright@gmail.com>
2021-11-04 09:41:26 -0400

    builds event and user controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,31 @@
 class EventsController < ApplicationController
+
+    def new
+        @event = Event.new
+    end
+
+    def create
+        @event = Event.new(event_params)
+        if @event.valid?
+            @event.save
+        else 
+            render 'new'  
+        end
+    end
+
+    def show
+        @event = Event.find(event_params)
+    end
+
+    def update
+    end
+
+    private
+
+    def event_params 
+        params.require(:event).permit(
+            :sourceIP,
+            :destinationIP
+        )
+    end
 end
